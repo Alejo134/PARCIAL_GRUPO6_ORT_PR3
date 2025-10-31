073d7df757edfdd60f126d06eab9074492fe5cc6
Lautaro Intraub <lautarointraub@hotmail.com>
2025-10-31 00:56:40 -0300

    .gitignore

--- app/src/main/java/com/example/parcial_pr3_ort/data/dao/LoginResponse.kt
+++
@@ -1,9 +0,0 @@
-package com.example.parcial_pr3_ort.data.dao
-
-import com.google.gson.annotations.SerializedName
-
-data class LoginResponse(
-    @SerializedName("token")
-    val token: String
-)
-    
